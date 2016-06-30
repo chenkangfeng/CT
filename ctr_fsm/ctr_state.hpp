5c14353a0ce786a53f668d009d0c42d42ded2bdd
chenkangfeng <593408166@qq.com>
2016-06-30 23:03:29 +0800

    111

--- ctr_fsm/ctr_state.hpp
+++ ctr_fsm/ctr_state.hpp
@@ -8,38 +8,107 @@
 #include "ctr_macro.h"
 #include "ctr_type.h"
 #include <functional>
+#include <typeinfo>
+
+#define state_transition(state1, state2) \
+public:                                  \
+typedef state1 from_type;                \
+typedef state2 to_type;
 
 ctr_namespace_begin
+
+class state_machine;
+class state_empty;
+
+class state_event
+{
+public:
+    state_transition(state_empty, state_empty)
+    
+    virtual ~state_event(void){}
+};
 
 class state_base
 {
 public:
     virtual ~state_base(void){}
     
-    virtual void enter(void) = 0;
-    virtual void exit(void) = 0;
-    virtual void update(ctr_float32) = 0;
+    virtual ctr_bool enter(void){return true;}
+    virtual ctr_bool exit(void){return true;}
+    virtual ctr_bool update(ctr_float32){return true;}
+    ctr_bool event(state_event&){return true;}
 };
 
-typedef std::function<void(void)> enter_func_type;
-typedef std::function<void(void)> exit_func_type;
-typedef std::function<void(ctr_float32)> update_func_type;
+class state_type
+{
+public:
+    template<typename type>
+    static ctr_bool is_equal(const state_base& state);
+    template<typename type1, typename type2>
+    static ctr_bool is_equal(void);
+    static ctr_bool is_equal(const state_base& state1, const state_base& state2)
+    {
+        return typeid(state1) == typeid(state2);
+    }
+};
 
-class state_root;
-class state_empty;
+template<typename type>
+ctr_bool state_type::is_equal(const state_base& state)
+{
+    return typeid(type) == typeid(state);
+}
+template<typename type1, typename type2>
+ctr_bool state_type::is_equal(void)
+{
+    return typeid(type1) == typeid(type2);
+}
 
-template<typename self, typename super = state_root, typename entry = state_empty>
+template<typename super = state_empty, typename entry = state_empty>
 class state : public state_base
 {
+    friend class state_machine;
 public:
     typedef super super_type;
     typedef entry entry_type;
     
+    state(void):super_state_(NULL){}
     virtual ~state(void){}
+
+    template<typename type>
+    type* state_ptr(void)
+    {
+        type* ret = NULL;
+        if(super_state_ != NULL){
+            if(state_type::is_equal<type>(*this)){
+                ret = dynamic_cast<type*>(this);
+            }else{
+                ret = super_state_->template state_ptr<type>();
+            }
+        }
+        return ret;
+    }
+protected:
+    super* super_state_;
+};
+
+class state_empty : public state<>{};
+
+template<typename self, typename super = state_empty, typename entry = state_empty>
+class state_ex : public state<super, entry>
+{
+public:
+    typedef std::function<ctr_bool(self&)> enter_func_type;
+    typedef std::function<ctr_bool(self&)> exit_func_type;
+    typedef std::function<ctr_bool(self&, ctr_float32)> update_func_type;
     
-    virtual void enter(void);
-    virtual void exit(void);
-    virtual void update(ctr_float32 dt);
+    typedef super super_type;
+    typedef entry entry_type;
+    
+    virtual ~state_ex(void){}
+    
+    virtual ctr_bool enter(void);
+    virtual ctr_bool exit(void);
+    virtual ctr_bool update(ctr_float32 dt);
     
     static enter_func_type enter_func;
     static exit_func_type exit_func;
@@ -47,36 +116,39 @@ public:
 };
 
 template<typename self, typename super, typename entry>
-enter_func_type state<self, super, entry>::enter_func = enter_func_type();
+typename state_ex<self, super, entry>::enter_func_type state_ex<self, super, entry>::enter_func = enter_func_type();
 template<typename self, typename super, typename entry>
-exit_func_type state<self, super, entry>::exit_func = exit_func_type();
+typename state_ex<self, super, entry>::exit_func_type state_ex<self, super, entry>::exit_func = exit_func_type();
 template<typename self, typename super, typename entry>
-update_func_type state<self, super, entry>::update_func = update_func_type();
+typename state_ex<self, super, entry>::update_func_type state_ex<self, super, entry>::update_func = update_func_type();
 
 template<typename self, typename super, typename entry>
-void state<self, super, entry>::enter(void)
+ctr_bool state_ex<self, super, entry>::enter(void)
 {
+    ctr_bool ret = true;
     if(enter_func){
-        enter_func();
+        ret = enter_func(*dynamic_cast<self*>(this));
     }
-}
+    return ret;
+};
 template<typename self, typename super, typename entry>
-void state<self, super, entry>::exit(void)
+ctr_bool state_ex<self, super, entry>::exit(void)
 {
+    ctr_bool ret = true;
     if(exit_func){
-        exit_func();
+        ret = exit_func(*dynamic_cast<self*>(this));
     }
-}
+    return ret;
+};
 template<typename self, typename super, typename entry>
-void state<self, super, entry>::update(ctr_float32 dt)
+ctr_bool state_ex<self, super, entry>::update(ctr_float32 dt)
 {
+    ctr_bool ret = true;
     if(update_func){
-        update_func(dt);
+        ret = update_func(*dynamic_cast<self*>(this), dt);
     }
-}
-
-class state_root : public state<state_root>{};
-class state_empty : public state<state_empty>{};
+    return ret;
+};
 
 ctr_namespace_end
 
--- ctr_fsm/ctr_state_machine.hpp
+++ ctr_fsm/ctr_state_machine.hpp
@@ -10,7 +10,6 @@
 #include <list>
 
 #include "ctr_state.hpp"
-#include "ctr_state_type.hpp"
 
 ctr_namespace_begin
 
@@ -18,89 +17,139 @@ class state_machine
 {
 public:
     state_machine(void){}
-    virtual ~state_machine(void);
+    virtual ~state_machine(void)
+    {
+        _delete_state<state_empty>();
+    }
     
     template<typename entry>
-    void start(void);
-    void update(ctr_float32 dt);
+    ctr_bool start(void);
+    void update(ctr_float32 dt)
+    {
+        for(auto itor = state_list_.begin();
+            itor != state_list_.end(); ++itor){
+            (*itor)->update(dt);
+        }
+    }
     template<typename type>
     ctr_bool is_state(void);
-    template<typename from, typename to>
-    void transition(void);
+    template<typename event, typename from, typename to>
+    ctr_bool transition(event& ev);
+    template<typename event>
+    ctr_bool send_event(event& ev);
 private:
     template<typename type>
-    void _alloc_state(void);
+    ctr_bool _alloc_state(void);
     template<typename type>
-    void _delete_state(void);
+    ctr_bool _delete_state(void);
+    template<typename type>
+    type* _get_state(void);
     
     std::list<state_base*> state_list_;
 };
 
-state_machine::~state_machine(void)
-{
-    _delete_state<state_root>();
-}
-
 template<typename entry>
-void state_machine::start(void)
-{
-    _alloc_state<entry>();
-}
-void state_machine::update(ctr_float32 dt)
-{
-    for(auto the_state:state_list_){
-        the_state->update(dt);
-    }
-}
-template<typename type>
-ctr_bool state_machine::is_state(void)
+ctr_bool state_machine::start(void)
 {
     ctr_bool ret = false;
-    for(auto itor = state_list_.rbegin();
-        itor != state_list_.rend(); ++itor){
-        if((ret = state_type::is_equal<type>(**itor))){
-            break;
+    if(state_list_.empty()){
+        if(state_type::is_equal<typename entry::super_type, state_empty>()){
+            ret = _alloc_state<entry>();
         }
     }
     return ret;
 }
-template<typename from, typename to>
-void state_machine::transition(void)
+template<typename type>
+ctr_bool state_machine::is_state(void)
 {
-    if(is_state<from>() &&
-       state_type::is_equal<typename from::super_type,
-       typename to::super_type>()){
-        _delete_state<from>();
-        _alloc_state<to>();
+    return _get_state<type>() != NULL;
+}
+template<typename event, typename from, typename to>
+ctr_bool state_machine::transition(event& ev)
+{
+    ctr_bool ret = false;
+    from* from_state = _get_state<from>();
+    if(from_state != NULL){
+        if(state_type::is_equal<from, to>()){
+            ret = from_state->event(ev);
+        }else{
+            if(state_type::is_equal<typename from::super_type, typename to::super_type>()){
+                if(from_state->event(ev)){
+                    if(_delete_state<from>()){
+                        if(_alloc_state<to>()){
+                            to* to_state = _get_state<to>();
+                            ret = (to_state != NULL && to_state->event(ev));
+                        }else{
+                            _delete_state<to>();
+                            _alloc_state<from>();
+                        }
+                    }
+                }
+            }
+        }
     }
+    return ret;
+}
+template<typename event>
+ctr_bool state_machine::send_event(event& ev)
+{
+    return transition<event, typename event::from_type, typename event::to_type>(ev);
 }
 
 template<typename type>
-void state_machine::_alloc_state(void)
+ctr_bool state_machine::_alloc_state(void)
 {
+    ctr_bool ret = false;
     if(!state_type::is_equal<type, state_empty>()){
-        state_base* the_state = new type();
-        the_state->enter();
-        state_list_.push_back(the_state);
-        if(!state_type::is_equal<type, typename type::enter_type>()){
-            _alloc_state<typename type::entry_type>();
+        type* the_state = new type();
+        if(state_list_.empty()){
+            the_state->super_state_ = NULL;
+        }else{
+            the_state->super_state_ = dynamic_cast<typename type::super_type*>(state_list_.back());
+        }
+        ret = the_state->enter();
+        if(ret){
+            state_list_.push_back(the_state);
+            if(!state_type::is_equal<typename type::entry_type, state_empty>()){
+                ret = (ret && _alloc_state<typename type::entry_type>());
+            }
+        }else{
+            delete the_state;
         }
     }
+    return ret;
 }
 template<typename type>
-void state_machine::_delete_state(void)
+ctr_bool state_machine::_delete_state(void)
 {
+    ctr_bool ret = false;
     if(!state_list_.empty()){
         state_base* the_state = state_list_.back();
         ctr_bool is_stop = (state_type::is_equal<type>(*the_state) ||
-                            state_type::is_equal<state_root>(*the_state));
-        the_state->exit();
-        state_list_.pop_back();
-        delete the_state;
-        if(!is_stop){
-            _delete_state<type>();
+                            state_type::is_equal<state_empty>(*the_state));
+        ret = the_state->exit();
+        if(ret){
+            state_list_.pop_back();
+            delete the_state;
+            if(!is_stop){
+                ret = (ret && _delete_state<type>());
+            }
         }
     }
+    return ret;
+}
+template<typename type>
+type* state_machine::_get_state(void)
+{
+    type* ret = NULL;
+    for(auto itor = state_list_.rbegin();
+        itor != state_list_.rend(); ++itor){
+        if(state_type::is_equal<type>(**itor)){
+            ret = dynamic_cast<type*>(*itor);
+            break;
+        }
+    }
+    return ret;
 }
 
 ctr_namespace_end
