d34d2027aae03ac9382812dc28d9bd70c66565af
chenkangfeng <593408166@qq.com>
2016-06-30 23:03:34 +0800

    111

--- ctr_net/ctr_dns.cpp
+++ ctr_net/ctr_dns.cpp
@@ -3,31 +3,198 @@
 //
 
 #include "ctr_dns.h"
+#include "event.h"
+#include "evdns.h"
 #include <string.h>
+#include <functional>
+#include <thread>
 
 using namespace std;
 
 ctr_namespace_begin
 
+class dns_asyn;
+class dns_asyn_stop;
+class dns_asyn_work;
+class dns_asyn_work_doing;
+class dns_asyn_work_succeed;
+class dns_asyn_work_failed;
+
+class dns_asyn_init : public state_event
+{
+    state_transition(dns_asyn, dns_asyn);
+    
+    state_machine* machine;
+};
+
+class dns_set_timeout : public state_event
+{
+public:
+    state_transition(dns_asyn, dns_asyn);
+    
+    timeval time;
+    std::function<void(void)> callback;
+};
+
+class dns_asyn_parse : public state_event
+{
+public:
+    state_transition(dns_asyn_stop, dns_asyn_work);
+    
+    ctr_strptr domain;
+    function<void(address&)> callback;
+};
+
+class dns_asyn_parse_succeed : public state_event
+{
+public:
+    state_transition(dns_asyn_work_doing, dns_asyn_work_succeed);
+    
+    evutil_addrinfo* result;
+};
+
+class dns_asyn_parse_failed : public state_event
+{
+public:
+    state_transition(dns_asyn_work_doing, dns_asyn_work_failed);
+};
+
+class dns_asyn : public state<state_empty, dns_asyn_stop>
+{
+public:
+    dns_asyn(void)
+    {
+        time.tv_sec = 3;
+        time.tv_usec = 0;
+    }
+    ctr_bool event(dns_asyn_init& ev)
+    {
+        machine = ev.machine;
+        return true;
+    }
+    ctr_bool event(dns_set_timeout& ev)
+    {
+        memcpy(&time, &ev.time, sizeof(ev.time));
+        timeout_callback = ev.callback;
+        return true;
+    }
+    
+    state_machine* machine;
+    
+    timeval time;
+    std::function<void(void)> timeout_callback;
+};
+
+class dns_asyn_stop : public state<dns_asyn>{};
+
+static void dns_timeout(evutil_socket_t, ctr_int16, void* param)
+{
+    if(param != NULL){
+        state_machine* machine = (state_machine*)param;
+        dns_asyn_parse_failed ev;
+        machine->send_event(ev);
+    }
+}
+
+static void dns_callback(ctr_int32 error, evutil_addrinfo* result, void* param)
+{
+    if(param != NULL){
+        state_machine* machine = (state_machine*)param;
+        dns_asyn_parse_succeed ev;
+        ev.result = result;
+        machine->send_event(ev);
+        evutil_freeaddrinfo(result);
+    }
+}
+
+class dns_asyn_work : public state<dns_asyn, dns_asyn_work_doing>
+{
+public:
+    dns_asyn_work(void)
+    {
+        ev_base = NULL;
+        ev_dns_base = NULL;
+        ev = NULL;
+    }
+    
+    virtual ctr_bool enter(void)
+    {
+        ev_base = event_base_new();
+        ev_dns_base = evdns_base_new(ev_base, 1);
+        ev = evtimer_new(ev_base, dns_timeout, super_state_->machine);
+        return true;
+    }
+    virtual ctr_bool exit(void)
+    {
+        event_free(ev);
+        evdns_base_free(ev_dns_base, 1);
+        event_base_free(ev_base);
+        return true;
+    }
+    ctr_bool event(dns_asyn_parse& ev)
+    {
+        domain = ev.domain;
+        parse_callback = ev.callback;
+        return true;
+    }
+    
+    struct event_base* ev_base;
+    struct evdns_base* ev_dns_base;
+    struct event* ev;
+    
+    ctr_strptr domain;
+    std::function<void(address&)> parse_callback;
+};
+
+static void dns_working(event_base* ev_base)
+{
+    event_base_dispatch(ev_base);
+}
+
+class dns_asyn_work_doing : public state<dns_asyn_work>
+{
+public:
+    virtual ctr_bool enter(void)
+    {
+        ctr_bool ret = false;
+        evutil_addrinfo hints;
+        //_init_domain(ev.domain, hints);
+        ret = (evdns_getaddrinfo(state_ptr<dns_asyn_work>()->ev_dns_base, state_ptr<dns_asyn_work>()->domain, NULL, &hints,
+                                 dns_callback, state_ptr<dns_asyn>()->machine) != NULL);
+        if(ret){
+            event_add(state_ptr<dns_asyn_work>()->ev, &state_ptr<dns_asyn>()->time);
+            working_thread = thread(dns_working, state_ptr<dns_asyn_work>()->ev_base);
+        }
+        return ret;
+    }
+    
+    std::thread working_thread;
+};
+
+class dns_asyn_work_succeed : public state<dns_asyn_work>
+{
+public:
+    virtual ctr_bool enter(void)
+    {
+        return true;
+    }
+};
+
+class dns_asyn_work_failed : public state<dns_asyn_work>
+{
+
+};
+
 dns::dns(void)
 {
-    enable_asyn_ = false;
-    is_working_ = false;
-    
-    ev_base_ = NULL;
-    ev_dns_base_ = NULL;
-    ev_ = NULL;
-    
-    time_.tv_sec = 3;
-    time_.tv_usec = 0;
+    machine_.start<dns_asyn>();
+    dns_asyn_init ev;
+    ev.machine = &machine_;
+    machine_.send_event(ev);
 }
 
 dns::~dns(void)
 {
-    if(thread_.joinable()){
-        thread_.join();
-    }
-    _enable_asyn(false);
 }
 
 ctr_bool dns::parse(ctr_strptr domain, address& addr)
@@ -44,112 +211,18 @@ ctr_bool dns::parse(ctr_strptr domain, address& addr)
 
 void dns::set_timeout(const timeval& time, const function<void(void)>& callback)
 {
-    memcpy(&time_, &time, sizeof(time));
-    timeout_callback_ = callback;
-}
-
-static void dns_callback(ctr_int32 error, evutil_addrinfo* result, void* param)
-{
-    if(param != NULL){
-        dns* dns_ins = (dns*)param;
-        address addr;
-        if(error == 0){
-            addr.init(result);
-        }
-        dns_ins->parse_callback(addr);
-        evutil_freeaddrinfo(result);
-    }
-}
-
-static void dns_working(event_base* ev_base)
-{
-    event_base_dispatch(ev_base);
+    dns_set_timeout ev;
+    ev.time = time;
+    ev.callback = callback;
+    machine_.send_event(ev);
 }
 
 ctr_bool dns::asyn_parse(ctr_strptr domain, const function<void(address&)>& callback)
 {
-    if(is_working_){
-        return false;
-    }
-    _enable_asyn(true);
-    
-    parse_callback_ = callback;
-    
-    evutil_addrinfo hints;
-    _init_domain(domain, hints);
-    ctr_bool ret = (evdns_getaddrinfo(ev_dns_base_, domain, NULL, &hints, dns_callback, this) != NULL);
-    if(ret){
-        is_working_ = true;
-        event_add(ev_, &time_);
-        thread_ = thread(dns_working, ev_base_);
-    }
-    return ret;
-}
-
-void dns::parse_callback(address& addr)
-{
-    if(mutex_.try_lock()){
-        if(is_working_){
-            if(parse_callback_){
-                parse_callback_(addr);
-            }
-            event_base_loopexit(ev_base_, NULL);
-            is_working_ = false;
-        }
-        mutex_.unlock();
-    }
-}
-
-void dns::timeout_callback(void)
-{
-    if(mutex_.try_lock()){
-        if(is_working_){
-            if(timeout_callback_){
-                timeout_callback_();
-            }
-            event_base_loopexit(ev_base_, NULL);
-            is_working_ = false;
-        }
-        mutex_.unlock();
-    }
-}
-
-static void dns_timeout(evutil_socket_t, ctr_int16, void* param)
-{
-    if(param != NULL){
-        dns* dns_ins = (dns*)param;
-        dns_ins->timeout_callback();
-    }
-}
-
-void dns::_enable_asyn(ctr_bool enable)
-{
-    if(enable && enable != enable_asyn_){
-        if(ev_base_ == NULL){
-            ev_base_ = event_base_new();
-        }
-        if(ev_dns_base_ == NULL){
-            ev_dns_base_ = evdns_base_new(ev_base_, 1);
-        }
-        if(ev_ == NULL){
-            ev_ = evtimer_new(ev_base_, dns_timeout, this);
-        }
-        enable_asyn_ = enable;
-    }else if(!enable && enable != enable_asyn_){
-        if(ev_ != NULL){
-            event_free(ev_);
-            ev_ = NULL;
-        }
-        if(ev_dns_base_){
-            evdns_base_free(ev_dns_base_, 1);
-            ev_dns_base_ = NULL;
-        }
-        if(ev_base_ != NULL){
-            event_base_free(ev_base_);
-            ev_base_ = NULL;
-        }
-        enable_asyn_ = enable;
-    }
+    dns_asyn_parse ev;
+    ev.domain = domain;
+    ev.callback = callback;
+    return machine_.send_event(ev);
 }
 
 void dns::_init_domain(ctr_strptr& domain, evutil_addrinfo& hints)
--- ctr_net/ctr_dns.h
+++ ctr_net/ctr_dns.h
@@ -7,12 +7,9 @@
 
 #include "ctr_macro.h"
 #include "ctr_type.h"
-#include "event.h"
-#include "evdns.h"
-#include <functional>
-#include <thread>
 
 #include "ctr_address.h"
+#include "ctr_state_machine.hpp"
 
 ctr_namespace_begin
 
@@ -26,26 +23,10 @@ public:
     
     void set_timeout(const timeval& time, const std::function<void(void)>& callback);
     ctr_bool asyn_parse(ctr_strptr domain, const std::function<void(address&)>& callback);
-    
-    void parse_callback(address& addr);
-    void timeout_callback(void);
 private:
-    void _enable_asyn(ctr_bool enable);
     void _init_domain(ctr_strptr& domain, evutil_addrinfo& hints);
     
-    ctr_bool enable_asyn_;
-    ctr_bool is_working_;
-    
-    event_base* ev_base_;
-    evdns_base* ev_dns_base_;
-    event* ev_;
-    
-    timeval time_;
-    std::function<void(void)> timeout_callback_;
-    
-    std::mutex mutex_;
-    std::thread thread_;
-    std::function<void(address&)> parse_callback_;
+    state_machine machine_;
 };
 
 ctr_namespace_end
